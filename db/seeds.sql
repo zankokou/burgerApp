d126e8ba18de296a0599d09494855234c5352f01
Ken <khuynh024@gmail.com>
2018-06-27 17:01:06 -0500

    app works need to work on visual

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,9 @@
 INSERT INTO burgers (burger_name)
 VALUES ('Cheese Burger'),('Apple Burger'),('Potato Burger');
 
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Most Delicious Burger Ever Burger', true);
+
+
 SELECT * FROM BURGERS;
